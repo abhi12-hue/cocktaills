54ab676f2ea5668c1a6a6c1cb2f5bcdf0df9273d
abhi12-hue <abhi12bisht@gmail.com>
2025-06-24 14:33:53 +0530

    commot

---
+++ clinet/src/App.jsx
@@ -0,0 +1,17 @@
+import React, { useEffect, useRef } from "react";
+import { ScrollTrigger, SplitText } from "gsap/all";
+import gsap from "gsap";
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
+function App() {
+  return (
+    <div className="flex-center h-[100vh]">
+      <h1 className="text-6xl font-bold underline">
+        Hello world!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
