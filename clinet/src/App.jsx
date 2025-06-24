98df8c8a02235fbec08ef4e6868ec09a6cd39cc5
abhi12-hue <abhi12bisht@gmail.com>
2025-06-24 15:44:15 +0530

    implement hero-section

--- clinet/src/App.jsx
+++ clinet/src/App.jsx
@@ -1,16 +1,18 @@
 import React, { useEffect, useRef } from "react";
 import { ScrollTrigger, SplitText } from "gsap/all";
 import gsap from "gsap";
+import NavBar from "./components/NavBar";
+import Hero from "./components/Hero";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 function App() {
   return (
-    <div className="flex-center h-[100vh]">
-      <h1 className="text-6xl font-bold underline">
-        Hello world!
-      </h1>
-    </div>
+    <main>
+      <NavBar/>
+      <Hero/>
+      <div className="h-dvh bg-black"></div>
+    </main>
   );
 }
 
